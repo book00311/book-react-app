bff91d8cb59f4df076ed478245256250eaff7666
unknown <papangkorn.boo@rmutto.ac.th>
2025-07-17 12:27:06 +0700

    frist lab02

---
+++ app/routes/book-lab02.tsx
@@ -0,0 +1,14 @@
+function MyProfile(){
+    return(
+        <div>
+            <p className="text-2xl">book profile</p>
+            <img src ="/images/Ghost.jpg" />
+            <p className="text-lg">
+                Name: Papangkorn boontam <br />
+                Student ID: 026730491002-4<br />
+            </p>
+        </div>
+    );
+}
+
+export default MyProfile;
